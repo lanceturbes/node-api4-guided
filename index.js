9c4d3d9334c3c99efcf084b0ba26c504131ea08b
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 09:56:42 -0700

    removed placeholder console logs

--- index.js
+++ index.js
@@ -9,19 +9,10 @@ app.get("/", (req, res) => {
     <h1>Web 47 rocks!</h1>
   `);
 });
+app.get("/port", (req, res) => {
+  res.send(`<p>Port is ${PORT}</p>`);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
-
-// eslint-disable-next-line
-function cohortRocks() {
-  console.log("arguments", process.argv.slice(2));
-  console.log(`web ${process.argv[2]} rocks!`);
-}
-function cohortRocksEnv() {
-  console.log(`My home folder is "${process.env.HOME}"`);
-  console.log(`My shell is "${process.env.SHELL}"`);
-  console.log(`In my environment, FOO is "${process.env.FOO}"`);
-}
-cohortRocksEnv();
