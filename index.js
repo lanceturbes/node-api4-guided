f2f703e5b50703212dd2500781413711f7c57e84
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 09:48:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Web 47 rocks!</h1>
+  `);
+});
+
+// eslint-disable-next-line
+function cohortRocks() {
+  console.log("arguments", process.argv.slice(2));
+  console.log(`web ${process.argv[2]} rocks!`);
+}
+function cohortRocksEnv() {
+  console.log(`My home folder is "${process.env.HOME}"`);
+  console.log(`My shell is "${process.env.SHELL}"`);
+  console.log(`In my environment, FOO is "${process.env.FOO}"`);
+}
+cohortRocksEnv();
